8e7c9081b25b9e19f0a23c1c56495b8d96a7013c
Hox0912 <hokeivan@gmail.com>
2024-12-11 11:20:03 +0100

    Authentication done with clerk so i don't have to do it on my own :)

--- src/home.jsx
+++ src/home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Button } from './components/ui/button'
+import { SignInButton } from '@clerk/clerk-react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+		<SignInButton mode='modal' forceRedirectUrl='/'>
+			<Button>Sign In</Button>
+		</SignInButton>
+    </div>
   )
 }
 
