7725b790062c749a87566e6150aa82866e76c0f1
Hox0912 <hokeivan@gmail.com>
2025-01-27 00:19:42 +0100

    edited home screen a little bit

--- src/components/MostSearchedCar.jsx
+++ src/components/MostSearchedCar.jsx
@@ -1,5 +1,5 @@
 import FakeData from '@/Shared/FakeData'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CarItem from './CarItem';
 import {
     Carousel,
@@ -8,17 +8,36 @@ import {
     CarouselNext,
     CarouselPrevious,
   } from "@/components/ui/carousel"
+import { db } from './../../configs';
+import { CarImages, CarListing } from './../../configs/schema';
+import { desc, eq } from 'drizzle-orm';
+import Service from '@/Shared/Service';
 
 function MostSearchedCar() {
-    console.log(FakeData.carList);
+
+	const [carList, setCarList]=useState([]);
+	useEffect(()=>{
+		GetPopularCarList();
+	},[])
+    
+	const GetPopularCarList=async()=>{
+		const result=await db.select().from(CarListing)
+			.leftJoin(CarImages,eq(CarListing.id,CarImages.carListingId))
+			.orderBy(desc(CarListing.id))
+			.limit(10)
+
+			const resp=Service.FormatResult(result);
+			console.log(resp);
+			setCarList(resp);
+	}
   return (
     <div className='mx-24'>
         <h2 className='font-bold text-3xl text-center mt-16 mb-7'>Napretrazivaniji auti</h2>
 
         <Carousel>
         <CarouselContent>
-            {FakeData.carList.map((car,index)=>(
-                <CarouselItem className="basis-1/4">
+            {carList.map((car,index)=>(
+                <CarouselItem key={index} className="basis-1/4 md:basis-1/3 lg:basis-1/4">
                     <CarItem car = {car}/>
                 </CarouselItem>     
             ))}
